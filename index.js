2035ce05114ac5fa39fc19cb4c5da6773afc616e
mk <v.a.stekolschikov@gmail.com>
2021-03-02 18:16:29 +0200

    add db

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 80
+const { Pool } = require('pg');
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
+})
+app.get('/db', async (req, res) => {
+    try {
+        const client = await pool.connect();
+        const result = await client.query('SELECT * FROM test_table');
+        const results = { 'results': (result) ? result.rows : null};
+        res.render('pages/db', results );
+        client.release();
+    } catch (err) {
+        console.error(err);
+        res.send("Error " + err);
+    }
 })
 
 app.listen(port, () => {
